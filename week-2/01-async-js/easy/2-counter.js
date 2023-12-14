2f7316f761f81c6a8cd4c37821f164db4efb7624
hashmat1530 <hashmat2020vskp@gmail.com>
2023-12-14 09:25:48 +0530

    solved 2 problems in easy module

---
+++ week-2/01-async-js/easy/2-counter.js
@@ -0,0 +1,86 @@
+/* Counter without setInterval
+
+Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+*/
+let counter=0;
+
+function counters(){
+setTimeout(()=>{
+    counter++;
+    console.log(counter);
+    counters()
+},1000)
+
+}
+
+counters()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (Hint: setTimeout)
